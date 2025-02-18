58076e88554efab87a28c08f4176f0376be6acd6
VIRUSI 🦠 MBAYA <virusimbayahiv33@gmail.com>
2025-02-18 23:35:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+    SESSION_ID: process.env.SESSION_ID,|| "s0JXGBJK#Sf7rjZp4oSQe3STtOPTDa1ozKWMiPwxivowVjp8jIOg"
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/rpmxu9.jpg"
     ALIVE_MSG: process.env.ALIVE_MSG ||  "Hello, iam alive" 
 };
